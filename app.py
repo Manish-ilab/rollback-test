90f2646375525af99082195649e650ef15adfa25
manish-ilab <manish@inspiringlab.com.np>
2025-10-09 12:24:51 +0545

    upgraded to v4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World – Version 3"
+    return "Hello World – Version 4,This is a demo project demonstrating rollback"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
