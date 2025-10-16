acfea0dbad16dfbac18445c0e324b6a02b466689
MSI <manish@inspiringlab.com.np>
2025-10-16 10:02:31 +0545

    modified app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World – Version 4,This is a demo project demonstrating rollback"
+    return "Hello World – Version 4,This is a demo project demonstrating rollback strategy"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
